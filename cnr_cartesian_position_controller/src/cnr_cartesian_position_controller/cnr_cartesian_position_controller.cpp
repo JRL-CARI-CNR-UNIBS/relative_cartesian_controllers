eabb3016ec027ea7c72cda0cfafbc29c088dbe23
Manuel Beschi <manuel.beschi@unibs.it>
2021-10-22 12:27:08 +0200

    moved from cnr ros controllers

---
+++ cnr_cartesian_position_controller/src/cnr_cartesian_position_controller/cnr_cartesian_position_controller.cpp
@@ -0,0 +1,486 @@
+#include <boost/algorithm/string.hpp>
+#include <ros/time.h>
+#include <tf/tf.h>
+#include <tf_conversions/tf_eigen.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <pluginlib/class_list_macros.h>
+#include <cnr_logger/cnr_logger_macros.h>
+
+#include <cnr_cartesian_position_controller/cnr_cartesian_position_controller.h>
+
+
+PLUGINLIB_EXPORT_CLASS(cnr::control::CartesianPositionController, controller_interface::ControllerBase)
+
+namespace cnr
+{
+namespace control
+{
+
+
+/**
+ * @brief CartesianPositionController::CartesianPositionController
+ */
+inline CartesianPositionController::CartesianPositionController()
+{
+}
+
+/**
+ * @brief CartesianPositionController::doInit
+ * @return
+ */
+inline bool CartesianPositionController::doInit()
+{
+  //INIT PUB/SUB
+  std::string setpoint_topic_name;
+  this->setKinUpdatePeriod(this->m_sampling_period); // superimposing the fkin_update_period,
+                                                     // we can then use chainCommand() sync and updated
+
+  this->setPriority(this->QD_PRIORITY);
+
+  this->setCommandVelocity(0.0*this->getVelocity()); //not needed, already superimposed in enterStarting()
+  this->setCommandPosition(this->getPosition());
+
+  if (!this->getControllerNh().getParam("max_cartesian_linear_speed",max_cart_lin_vel_))
+  {
+    CNR_INFO(this->logger(),this->getControllerNamespace()<<"/max_cartesian_linear_speed not defined, using 0.25 m/s");
+    max_cart_lin_vel_=0.25;
+  }
+
+  if (!this->getControllerNh().getParam("max_cartesian_linear_acceleration",max_cart_lin_acc_))
+  {
+    CNR_INFO(this->logger(),this->getControllerNamespace()<<"/max_cartesian_linear_acceleration not defined, using 0.75 m/s^2");
+    max_cart_lin_acc_=0.75;
+  }
+
+  if (!this->getControllerNh().getParam("max_cartesian_angular_speed",max_cart_ang_vel_))
+  {
+    CNR_INFO(this->logger(),this->getControllerNamespace()<<"/max_cartesian_angular_speed not defined, using 0.5 rad/s");
+    max_cart_ang_vel_=0.5;
+  }
+
+  if (!this->getControllerNh().getParam("max_cartesian_angular_acceleration",max_cart_ang_acc_))
+  {
+    CNR_INFO(this->logger(),this->getControllerNamespace()<<"/max_cartesian_angular_acceleration not defined, using 1.5 rad/s^2");
+    max_cart_ang_acc_=1.5;
+  }
+
+  if (!this->getControllerNh().getParam("click_gain",m_clik_gain))
+  {
+    CNR_INFO(this->logger(),this->getControllerNamespace()<<"/click_gain not defined, using 5.0");
+    m_clik_gain=5.0;
+  }
+
+  if (!this->getControllerNh().getParam("check_actual_configuration",check_actual_configuration_))
+  {
+    CNR_INFO(this->logger(),this->getControllerNamespace()<<"/check_actual_configuration not defined, set true");
+    check_actual_configuration_=true;
+  }
+
+  as_.reset(new actionlib::ActionServer<cnr_cartesian_position_controller::RelativeMoveAction>(this->getControllerNh(), "relative_move",
+                                                                      boost::bind(&CartesianPositionController::actionGoalCallback,    this,  _1),
+                                                                      boost::bind(&CartesianPositionController::actionCancelCallback,  this,  _1),
+                                                                      false));
+  as_->start();
+
+  tool_name_=this->chain().getLinksName().back();
+
+  tf::StampedTransform tf_base_tool;
+  try
+  {
+    listener_.waitForTransform ( this->chain().getLinksName().front(),
+                                 tool_name_,
+                                 ros::Time(0),
+                                 ros::Duration ( 10.0 ) );
+    listener_.lookupTransform(this->chain().getLinksName().front(),
+                              tool_name_,
+                              ros::Time(0),
+                              tf_base_tool
+                              );
+  }
+  catch (std::exception& e)
+  {
+    CNR_ERROR(this->logger(),"unable to lookup tf tree, reason ="<<e.what());
+    CNR_RETURN_FALSE(this->logger());
+  }
+  CNR_RETURN_TRUE(this->logger());
+}
+
+/**
+ * @brief CartesianPositionController::doStarting
+ * @param time
+ */
+inline bool CartesianPositionController::doStarting(const ros::Time& /*time*/)
+{
+  CNR_TRACE_START(this->logger(),"Starting Controller");
+  this->setCommandVelocity(0.0*this->getVelocity()); //not needed, already superimposed in enterStarting()
+  this->setCommandPosition(this->getPosition());
+
+  mtx_.lock();
+  rosdyn::VectorXd q = this->getPosition();
+  T_base_setpoint_=this->chainNonConst().getTransformation(q);
+  T_base_destination_=T_base_setpoint_;
+  T_base_actual_=T_base_setpoint_;
+  mtx_.unlock();
+
+  last_twist_of_setpoint_in_base_ = Eigen::Vector6d::Zero();
+  CNR_RETURN_TRUE(this->logger());
+}
+
+/**
+ * @brief CartesianPositionController::stopping
+ * @param time
+ */
+inline bool CartesianPositionController::doStopping(const ros::Time& /*time*/)
+{
+  CNR_TRACE_START(this->logger(),"Stopping Controller");
+
+  stop_thread_=true;
+  if (m_as_thread.joinable())
+  {
+    m_as_thread.join();
+  }
+  CNR_RETURN_TRUE(this->logger());
+}
+
+/**
+ * @brief CartesianPositionController::doUpdate
+ * @param time
+ * @param period
+ * @return
+ */
+inline bool CartesianPositionController::doUpdate(const ros::Time& /*time*/, const ros::Duration& period)
+{
+  CNR_TRACE_START_THROTTLE_DEFAULT(this->logger());
+  std::stringstream report;
+
+
+  mtx_.lock();
+  rosdyn::VectorXd old_vel_sp = this->getCommandVelocity();
+  rosdyn::VectorXd pos_sp = this->getCommandPosition();
+  T_base_setpoint_=this->chainNonConst().getTransformation(pos_sp);
+  rosdyn::VectorXd q = this->getPosition();
+  T_base_actual_=this->chainNonConst().getTransformation(q);
+  Eigen::Affine3d T_base_destination=T_base_destination_;
+  mtx_.unlock();
+
+  Eigen::Vector6d distance_in_base;
+  rosdyn::getFrameDistance(T_base_destination,T_base_setpoint_,distance_in_base);
+
+  Eigen::Vector6d twist_of_setpoint_in_base = m_clik_gain*distance_in_base;
+
+  if (twist_of_setpoint_in_base.head(3).norm() > max_cart_lin_vel_)
+    twist_of_setpoint_in_base *= max_cart_lin_vel_/twist_of_setpoint_in_base.head(3).norm();
+
+  if (twist_of_setpoint_in_base.head(3).norm() > target_linear_velocity_)
+    twist_of_setpoint_in_base *= target_linear_velocity_/twist_of_setpoint_in_base.head(3).norm();
+
+  if (twist_of_setpoint_in_base.tail(3).norm()>max_cart_ang_vel_)
+    twist_of_setpoint_in_base*=max_cart_ang_vel_/twist_of_setpoint_in_base.tail(3).norm();
+
+  if (twist_of_setpoint_in_base.tail(3).norm() > target_angular_velocity_)
+    twist_of_setpoint_in_base *= target_angular_velocity_/twist_of_setpoint_in_base.tail(3).norm();
+
+  Eigen::Vector6d Dtwist_of_setpoint_in_base;
+  if (period.toSec()>0.0)
+  {
+    Dtwist_of_setpoint_in_base = (twist_of_setpoint_in_base-last_twist_of_setpoint_in_base_)/period.toSec();
+    double scaling=1.0;
+    if (Dtwist_of_setpoint_in_base.head(3).norm()>max_cart_lin_acc_)
+      scaling=max_cart_lin_acc_/Dtwist_of_setpoint_in_base.head(3).norm();
+
+    if (Dtwist_of_setpoint_in_base.tail(3).norm()>max_cart_ang_acc_)
+      scaling=std::min(scaling,max_cart_ang_acc_/Dtwist_of_setpoint_in_base.tail(3).norm());
+
+    Dtwist_of_setpoint_in_base*=scaling;
+    twist_of_setpoint_in_base=last_twist_of_setpoint_in_base_+Dtwist_of_setpoint_in_base*period.toSec();
+  }
+  else
+  {
+    twist_of_setpoint_in_base = Eigen::Vector6d::Zero( );
+    last_twist_of_setpoint_in_base_ = Eigen::Vector6d::Zero( );
+  }
+
+  Eigen::Matrix6Xd J_of_setpoint_in_base;
+  J_of_setpoint_in_base=this->chainCommand().toolJacobian();  // CHECK IF CORRECT
+
+  Eigen::FullPivLU<Eigen::MatrixXd> pinv_J(J_of_setpoint_in_base);
+
+  pinv_J.setThreshold ( 1e-2 );
+
+
+  Eigen::JacobiSVD<Eigen::MatrixXd> svd(J_of_setpoint_in_base, Eigen::ComputeThinU | Eigen::ComputeThinV);
+  auto sv = svd.singularValues();
+  CNR_WARN_COND_THROTTLE(this->logger(),
+                          (sv(sv.rows()-1)==0) || (sv(0)/sv(sv.rows()-1) > 1e2), 2, "SINGULARITY POINT" );
+
+  if(pinv_J.rank()<6)
+  {
+    CNR_WARN_THROTTLE(this->logger(),2,"rank: "<<pinv_J.rank()<<"\nJacobian\n"<<J_of_setpoint_in_base);
+    singularity_=true;
+  }
+
+  rosdyn::VectorXd vel_sp = svd.solve(twist_of_setpoint_in_base);
+
+  if(rosdyn::saturateSpeed(this->chainNonConst(),vel_sp,old_vel_sp,
+                           this->getCommandPosition(),period.toSec(), 1.0, true, &report)) // CHECK!
+  {
+    CNR_DEBUG_THROTTLE(this->logger(), 2.0, "\n" << report.str() );
+  }
+
+  Eigen::Vector6d twist_of_t_in_bcommand=J_of_setpoint_in_base*vel_sp;
+  Eigen::Vector6d versor=twist_of_setpoint_in_base.normalized();
+  Eigen::Vector6d parallel_twist=twist_of_t_in_bcommand.dot(versor)*versor;
+  Eigen::Vector6d perpendicular_twist=twist_of_t_in_bcommand -parallel_twist;
+
+  if (perpendicular_twist.norm()>1e-6)
+  {
+    vel_sp*=1e-6/perpendicular_twist.norm();
+    CNR_WARN_THROTTLE(this->logger(),1,"saturating velocity, direction error (perpendicular norm = " << perpendicular_twist.norm() <<  ") due to singularity and joint limits");
+    CNR_DEBUG_THROTTLE(this->logger(),1,
+                      "twist_of_t_in_b         = " << twist_of_setpoint_in_base.transpose() << std::endl <<
+                      "twist_of_t_in_bcommand = " << twist_of_t_in_bcommand.transpose() << std::endl <<
+                      "parallel_twist velocity = " << parallel_twist.transpose() << std::endl <<
+                      "perpedicular velocity   = " << perpendicular_twist.transpose()
+                      );
+  }
+  last_twist_of_setpoint_in_base_=J_of_setpoint_in_base*vel_sp;
+
+  if(rosdyn::saturateSpeed(this->chainNonConst(),vel_sp,old_vel_sp,
+                              this->getCommandPosition(),period.toSec(), 1.0, true, &report)) // CHECK!
+  {
+    CNR_DEBUG_THROTTLE(this->logger(), 2.0, "\n" << report.str() );
+  }
+
+
+  pos_sp = this->getCommandPosition() + vel_sp * period.toSec();
+
+  if(rosdyn::saturatePosition(this->chainNonConst(),pos_sp, &report))
+  {
+    CNR_DEBUG_THROTTLE(this->logger(), 2.0, "\n" << report.str() );
+  }
+
+  last_twist_of_setpoint_in_base_=J_of_setpoint_in_base*vel_sp;
+  this->setCommandPosition( pos_sp );
+  this->setCommandVelocity( vel_sp );
+
+  CNR_RETURN_TRUE_THROTTLE_DEFAULT(this->logger());
+}
+
+/**
+ * @brief CartesianPositionController::actionGoalCallback
+ * @param gh
+ */
+void CartesianPositionController::actionGoalCallback(actionlib::ActionServer< cnr_cartesian_position_controller::RelativeMoveAction>::GoalHandle gh)
+{
+  try
+  {
+    if (gh_)
+    {
+      CNR_INFO(this->logger(),"preepmt previous goal");
+      stop_thread_=true;
+    }
+    auto goal = gh.getGoal();
+    singularity_=false;
+    std::shared_ptr<actionlib::ActionServer<cnr_cartesian_position_controller::RelativeMoveAction>::GoalHandle> current_gh;
+
+    current_gh.reset(new actionlib::ActionServer<cnr_cartesian_position_controller::RelativeMoveAction>::GoalHandle(gh));
+    gh_ = current_gh;
+
+
+    Eigen::Affine3d T_setpoint_destination;
+    tf::poseMsgToEigen(goal->relative_pose.pose,T_setpoint_destination);
+    if (goal->relative_pose.header.frame_id=="TOOL" || goal->relative_pose.header.frame_id==tool_name_)
+    {
+      tf::poseMsgToEigen(goal->relative_pose.pose,T_setpoint_destination);
+    }
+    else
+    {
+      tf::StampedTransform tf_tool_frame;
+      try
+      {
+        listener_.waitForTransform ( tool_name_,
+                                     goal->relative_pose.header.frame_id,
+                                     ros::Time(0),
+                                     ros::Duration ( 10.0 ) );
+        listener_.lookupTransform(tool_name_,
+                                  goal->relative_pose.header.frame_id,
+                                  ros::Time(0),
+                                  tf_tool_frame);
+      }
+      catch (std::exception& e)
+      {
+        CNR_ERROR(this->logger(),
+                  "unable to find a transformation tool("<<
+                  tool_name_<<
+                  ") <== destination("<<
+                  goal->relative_pose.header.frame_id<<
+                  "). reason = " <<
+                  e.what());
+        T_setpoint_destination.setIdentity();
+        cnr_cartesian_position_controller::RelativeMoveResult result;
+        result.error_code   = -1;
+        result.error_string = "invalid frame id";
+        gh_->setRejected(result);
+        return;
+      }
+
+      Eigen::Affine3d T_tool_frame;
+      tf::poseTFToEigen(tf_tool_frame,T_tool_frame);
+
+      Eigen::Affine3d T_setpoint_destination_in_frame;
+      tf::poseMsgToEigen(goal->relative_pose.pose,T_setpoint_destination_in_frame);
+      Eigen::AngleAxisd aa_in_frame(T_setpoint_destination_in_frame.linear());
+      Eigen::Vector3d axis_in_frame=aa_in_frame.axis();
+      double angle=aa_in_frame.angle();
+      Eigen::Vector3d axis_in_t=T_tool_frame.linear()*axis_in_frame;
+
+      Eigen::AngleAxisd aa_in_setpoint(angle,axis_in_t);
+      T_setpoint_destination=aa_in_setpoint;
+
+      Eigen::Vector3d p_destination_in_frame=T_setpoint_destination_in_frame.translation();
+      Eigen::Vector3d p_destination_in_setpoint=T_tool_frame.linear()*p_destination_in_frame;
+      T_setpoint_destination.translation()=p_destination_in_setpoint;
+    }
+
+    target_linear_velocity_=goal->target_linear_velocity;
+    if (target_linear_velocity_<=0)
+    {
+      CNR_ERROR(this->logger(),"target linear velocity should be positive");
+      T_setpoint_destination.setIdentity();
+      cnr_cartesian_position_controller::RelativeMoveResult result;
+      result.error_code   = -2;
+      result.error_string = "target linear velocity should be positive";
+      gh_->setRejected(result);
+      return;
+    }
+    target_angular_velocity_=goal->target_linear_velocity;
+    if (target_angular_velocity_<=0)
+    {
+      CNR_ERROR(this->logger(),"target angular velocity should be positive");
+      T_setpoint_destination.setIdentity();
+      cnr_cartesian_position_controller::RelativeMoveResult result;
+      result.error_code   = -2;
+      result.error_string = "target angular velocity should be positive";
+      gh_->setRejected(result);
+      return;
+    }
+    CNR_INFO(this->logger(),"[ "<<this->getControllerNamespace()<<" ] New Goal Received, action start!");
+    gh_->setAccepted();
+
+    mtx_.lock();
+    T_base_destination_=T_base_setpoint_*T_setpoint_destination;
+    mtx_.unlock();
+
+    if (m_as_thread.joinable())
+    {
+      m_as_thread.join();
+    }
+
+    m_as_thread    = std::thread(&CartesianPositionController::actionThreadFunction,this);
+  }
+  catch( std::exception& e )
+  {
+    CNR_ERROR(this->logger(),"Exception. what: " << e.what() );
+    cnr_cartesian_position_controller::RelativeMoveResult result;
+    result.error_code   = -1;
+    result.error_string = std::string("exception: ")+e.what();
+    gh_->setAborted(result);
+  }
+  catch( ... )
+  {
+    CNR_ERROR(this->logger(),"Generalized Exception.");
+    cnr_cartesian_position_controller::RelativeMoveResult result;
+    result.error_code   = -1;
+    result.error_string = "goal exception";
+    gh_->setAborted(result);
+  }
+
+}
+
+void CartesianPositionController::actionCancelCallback(actionlib::ActionServer< cnr_cartesian_position_controller::RelativeMoveAction >::GoalHandle /*gh*/)
+{
+  if (gh_)
+  {
+    gh_->setCanceled();
+    stop_thread_ = true;
+    if (m_as_thread.joinable())
+    {
+      m_as_thread.join();
+    }
+    gh_.reset();
+  }
+  else
+  {
+    CNR_WARN(this->logger(),"Triggered the Cancel of the Action but none Goal is active.");
+  }
+}
+
+void CartesianPositionController::actionThreadFunction()
+{
+  ros::WallRate lp(100);
+
+  ros::Time leaving_timer;
+
+  while (this->getControllerNh().ok())
+  {
+    lp.sleep();
+    if (!gh_)
+    {
+      CNR_ERROR(this->logger(),"Goal handle is not initialized");
+      break;
+    }
+    cnr_cartesian_position_controller::RelativeMoveFeedback fb;
+    cnr_cartesian_position_controller::RelativeMoveResult result;
+    mtx_.lock();
+    Eigen::Affine3d T_base_setpoint=T_base_setpoint_;
+    Eigen::Affine3d T_base_actual=T_base_actual_;
+    Eigen::Affine3d T_b_destination=T_base_destination_;
+    mtx_.unlock();
+
+    Eigen::Vector6d distance;
+    if (check_actual_configuration_)
+      rosdyn::getFrameDistance(T_b_destination,T_base_actual,distance);
+    else
+      rosdyn::getFrameDistance(T_b_destination,T_base_setpoint,distance);
+
+    if (distance.head(3).norm()<m_lin_tolerance && distance.tail(3).norm()<m_ang_tolerance)
+    {
+      result.error_code   = 0;
+      result.error_string = "finished";
+      gh_->setSucceeded(result);
+      break;
+    }
+
+    if( stop_thread_ )
+    {
+      CNR_ERROR(this->logger(),"Triggered an external stop. Break the action loop.");
+      result.error_code   = -4;
+      result.error_string = "Cancelled";
+      mtx_.lock();
+      T_base_destination_=T_base_setpoint_;
+      mtx_.unlock();
+      gh_->setAborted(result);
+      break;
+    }
+
+    if (singularity_)
+    {
+      CNR_ERROR(this->logger(),"Singularity");
+      result.error_code   = -3;
+      result.error_string = "Singularity";
+      gh_->setAborted(result);
+      mtx_.lock();
+      T_base_destination_=T_base_setpoint_;
+      mtx_.unlock();
+      break;
+    }
+
+    gh_->publishFeedback(fb);
+  }
+  gh_.reset();
+}
+
+
+}
+}
